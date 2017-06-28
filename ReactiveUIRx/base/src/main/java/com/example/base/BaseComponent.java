bc2241a735230fa931e3356cf0ddcc46f554c748
Linh <Linh@Truong.local>
2017-06-28 17:30:56 +0700

    delete all

--- ReactiveUIRx/base/src/main/java/com/example/base/BaseComponent.java
+++
@@ -1,9 +0,0 @@
-package com.example.base;
-
-/**
- * Created by ngoclinh.truong on 6/14/16.
- */
-public interface BaseComponent {
-    void inject(BaseActivity activity);
-    void inject(BaseFragment fragment);
-}
